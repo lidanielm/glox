9770438d4f411ac055dc835ee4e50b1baf72b58f
DLi2004 <danielmli2023@gmail.com>
2025-05-24 11:37:12 -0400

    Add visitor patterns to generateAst

--- src/pkg/internal/tool/generateAst.go
+++ src/pkg/internal/tool/generateAst.go
@@ -7,9 +7,9 @@ import (
 )
 
 func main() {
-	outputDir := "out"
+	outputDir := "src/pkg/internal/tool/out"
 	baseName := "Expr"
-	types := []string{"Binary : Expr left, Token operator, Expr right"}
+	types := []string{"Binary : left Expr, operator Token, right Expr", "Grouping : expression Expr", "Literal : value Object", "Unary : operator Token, right Expr"}
 	err := defineAst(outputDir, baseName, types)
 	if err != nil {
 		panic(err)
@@ -26,35 +26,60 @@ func defineAst(outputDir string, baseName string, types []string) (err error) {
 
 	fmt.Fprintf(file, "package " + outputDir + "\n\n")
 	fmt.Fprintf(file, "type " + baseName + " interface {\n")
-
+	fmt.Fprintf(file, "\tAccept(visitor Visitor[any]) any\n")
 	fmt.Fprintf(file, "}\n\n")
 
+	// Define classes
 	for _, typ := range types {
 		className := strings.TrimSpace(strings.Split(typ, ":")[0])
+		classType := strings.TrimSpace(strings.Split(typ, ":")[1])
 		fields := strings.Split(strings.TrimSpace(strings.Split(typ, ":")[1]), ", ")
-		fmt.Fprintf(file, "type " + className + " struct {\n")
-	
-		for _, field := range fields {
-			name, ftype := strings.Split(field, " ")[0], strings.Split(field, " ")[1]
-			fmt.Fprintf(file, "\t%v %v\n", name, ftype)
-		}
-
-		fmt.Fprintf(file, "}\n")
-		
-		fmt.Fprintln(file, "")
-
-		fmt.Fprintf(file, "func New%v(%v) *%v {\n", baseName, strings.TrimSpace(strings.Split(typ, ":")[1]), className)		
-		fmt.Fprintf(file, "\treturn &%v{", className)
-		for index, field := range fields {
-			name, ftype := strings.Split(field, " ")[0], strings.Split(field, " ")[1]
-			fmt.Fprintf(file, "%v: %v", name, ftype)
-			if index < len(fields) - 1 {
-				fmt.Fprint(file, ", ")
-			}
-		}
-		fmt.Fprintf(file, "}\n")
-		fmt.Fprintf(file, "}\n")
+		defineType(file, baseName, className, classType, fields)
 	}
 
+	return nil
+}
+
+func defineType(file *os.File, baseName string, className string, classType string, fields []string) {
+	// Define struct type
+	fmt.Fprintf(file, "type " + className + " struct {\n")
+
+	for _, field := range fields {
+		name, ftype := strings.Split(field, " ")[0], strings.Split(field, " ")[1]
+		fmt.Fprintf(file, "\t%v %v\n", name, ftype)
+	}
+
+	fmt.Fprintf(file, "}\n")
+	
+	fmt.Fprintln(file, "")
+
+	// Define constructor
+	fmt.Fprintf(file, "func New%v(%v) *%v {\n", className, classType, className)		
+	fmt.Fprintf(file, "\treturn &%v{", className)
+	for index, field := range fields {
+		name, ftype := strings.Split(field, " ")[0], strings.Split(field, " ")[1]
+		fmt.Fprintf(file, "%v: %v", name, ftype)
+		if index < len(fields) - 1 {
+			fmt.Fprint(file, ", ")
+		}
+	}
+	fmt.Fprintf(file, "}\n")
+	fmt.Fprintf(file, "}\n\n")
+
+	// Define visitor method
+	fmt.Fprintf(file, "func (%c %v) Accept(visitor Visitor[any]) any {\n", strings.ToLower(className)[0], className)
+	fmt.Fprintf(file, "\treturn visitor.visit%v%v(%c)\n", className, baseName, strings.ToLower(className)[0])
+	fmt.Fprintf(file, "}\n\n")
+}
+
+func defineVisitor(file *os.File, baseName string, types []string) (err error) {
+	fmt.Fprintf(file, "type Visitor[R any] interface {\n")
+	
+	for _, typ := range types {
+		typeName := strings.TrimSpace(strings.Split(typ, ":")[0])
+		fmt.Fprintf(file, "\tVisit%v%v(%v %v) R\n", typeName, baseName, strings.ToLower(baseName), typeName)
+	}
+
+	fmt.Fprintf(file, "}\n\n")
 	return nil
 }
--- src/pkg/internal/tool/out/Expr.go
+++ src/pkg/internal/tool/out/Expr.go
@@ -1,14 +1,57 @@
-package out
+package src/pkg/internal/tool/out
 
 type Expr interface {
+	Accept(visitor Visitor[any]) any
 }
 
 type Binary struct {
-	Expr left
-	Token operator
-	Expr right
+	left Expr
+	operator Token
+	right Expr
 }
 
-func NewExpr(Expr left, Token operator, Expr right) *Binary {
-	return &Binary{Expr: left, Token: operator, Expr: right}
+func NewBinary(left Expr, operator Token, right Expr) *Binary {
+	return &Binary{left: Expr, operator: Token, right: Expr}
 }
+
+func (b Binary) Accept(visitor Visitor[any]) any {
+	return visitor.visitBinaryExpr(b)
+}
+
+type Grouping struct {
+	expression Expr
+}
+
+func NewGrouping(expression Expr) *Grouping {
+	return &Grouping{expression: Expr}
+}
+
+func (g Grouping) Accept(visitor Visitor[any]) any {
+	return visitor.visitGroupingExpr(g)
+}
+
+type Literal struct {
+	value Object
+}
+
+func NewLiteral(value Object) *Literal {
+	return &Literal{value: Object}
+}
+
+func (l Literal) Accept(visitor Visitor[any]) any {
+	return visitor.visitLiteralExpr(l)
+}
+
+type Unary struct {
+	operator Token
+	right Expr
+}
+
+func NewUnary(operator Token, right Expr) *Unary {
+	return &Unary{operator: Token, right: Expr}
+}
+
+func (u Unary) Accept(visitor Visitor[any]) any {
+	return visitor.visitUnaryExpr(u)
+}
+
